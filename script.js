18e9eb1c0164590ab209ba197e3adff7a7cff26a
VviratT <123719148+VviratT@users.noreply.github.com>
2024-07-21 01:20:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
         if(boxVal){
             boxVal = false;
+            box.classList.add("addColor");
             box.innerText = 'O';
         }
         else{
